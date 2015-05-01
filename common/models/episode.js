5cccb1bae7120e53babe3503e89ff833cf00fcde
neemzy <tom.panier@free.fr>
2015-05-01 23:02:45 +0200

    added validation rules to episodes

--- common/models/episode.js
+++ common/models/episode.js
@@ -1,3 +1,28 @@
 module.exports = function(Episode) {
+    'use strict';
 
+    // Season number cannot be 0
+    Episode.validate('season', function (err) {
+        if (1 > this.season) {
+            err();
+        }
+    });
+
+    // Episode number cannot be 0
+    Episode.validate('number', function (err) {
+        if (1 > this.number) {
+            err();
+        }
+    });
+
+    // Episode must be linked to a valid Show
+    Episode.validateAsync('showId', function (err, done) {
+        this.show(function (error, show) {
+            if (error || null === show) {
+                err();
+            }
+
+            done();
+        });
+    });
 };
